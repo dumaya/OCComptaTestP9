2129f636594bf843b48d8546a7a05dbdf1f852b3
Alexis Dumay S627639 <alexis.dumay@axa.fr>
2020-04-29 15:01:40 +0200

    correction

--- myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/SequenceEcritureComptableTest.java
+++ myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/SequenceEcritureComptableTest.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class SequenceEcritureComptableTest {
     @Test
     public void getterSetter() {
-        SequenceEcritureComptable sequence = new SequenceEcritureComptable(5,6);
+        SequenceEcritureComptable sequence = new SequenceEcritureComptable("GK",5,6);
         Assert.assertEquals(5,sequence.getAnnee().intValue());
         Assert.assertEquals(6,sequence.getDerniereValeur().intValue());
         Assert.assertEquals("SequenceEcritureComptable{annee=5, derniereValeur=6}",sequence.toString());
